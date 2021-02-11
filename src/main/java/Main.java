cf3225bbb2d78a44b96804f7bbda0a8bc0b216f0
Abdi Abdulle <abdi.abdulle@outlook.dk>
2021-02-11 20:50:06 +0100

    couple of souts added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,24 @@
+import java.util.Date;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        Date date = new Date();
         Customer c1 = new Customer("Jon");
         Account account = new Account(c1);
+
+
 
         int newBalance = account.depositAmount(125);
         System.out.println(String.format("Ny balance: %d", newBalance));
 
         newBalance = account.depositAmount(325);
         System.out.println(String.format("Ny balance: %d", newBalance));
+
+        System.out.println(account.getTransactions());
+        System.out.println("Balance -" + account.getBalance());
+        System.out.println("Withdrawal amount - " + account.withDrawAmount(20));
 
     }
 }
