7465b70150862fe04050190c3f11c23d3b3f8a83
Abdi Abdulle <abdi.abdulle@outlook.dk>
2021-02-11 20:50:43 +0100

    toString override

--- src/main/java/Transaction.java
+++ src/main/java/Transaction.java
@@ -17,4 +17,11 @@ public class Transaction {
     public Date getDate() {
         return date;
     }
+
+    @Override
+    public String toString() {
+        return "Transaction - " +
+                "amount: " + amount +
+                " date: " + date + "\n";
+    }
 }
