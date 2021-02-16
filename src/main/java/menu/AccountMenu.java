85238ffad6813c1e195087bc758dad26907cbc94
Mathias2860DK <mathias.enemark.poulsen@gmail.com>
2021-02-16 11:41:06 +0100

    updated accountMenu

--- src/main/java/menu/AccountMenu.java
+++ src/main/java/menu/AccountMenu.java
@@ -4,8 +4,9 @@ public class AccountMenu implements Menu{
     @Override
     public void menu() {
         System.out.println("1) Tilføj penge");
-        System.out.println("4) Hæve penge");
+        System.out.println("2) Hæve penge");
         System.out.println("3) Se alle kontoudtog");
         System.out.println("4) Se saldo");
+        System.out.println("9) Exit");
     }
 }
