668cb8475e0445020ef26428d300a65225f44cf5
Mathias2860DK <mathias.enemark.poulsen@gmail.com>
2021-02-19 09:59:29 +0100

    added Comparable interface in bankansat and a method which prints the list of konti ordered

--- src/main/java/data/CustomerMapper.java
+++ src/main/java/data/CustomerMapper.java
@@ -3,11 +3,32 @@ package data;
 import domæne.Customer;
 import domæne.CustomerInterface;
 
-public class CustomerMapper implements CustomerInterface {
+public class CustomerMapper implements CustomerInterface, Comparable<CustomerMapper> {
+    private int id;
+    private String name;
+    private String by;
+
+    public CustomerMapper(int id, String name, String by){
+        this.id = id;
+        this.name = name;
+        this.by = by;
+    }
 
 
     @Override
     public Customer findCustomerByLogin(String name) {
         return new Customer(3,"Alex");
+    }
+
+    @Override
+    public String toString() {
+        return  "id = " + id +
+                ", name = " + name +
+                ", by = " + by ;
+    }
+
+    @Override
+    public int compareTo(CustomerMapper o) {
+        return o.name.compareTo(this.name);
     }
 }
--- src/main/java/domæne/Bankansat.java
+++ src/main/java/domæne/Bankansat.java
@@ -1,30 +1,45 @@
 package domæne;
 
 import ConnectionToDB.ConnectionDB;
+import data.CustomerMapper;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Bankansat {
     Scanner scanner = new Scanner(System.in);
     Account account = new Account();
+    List<CustomerMapper> customerMapperList;
 
-    public void seAlleKonti() throws SQLException {
+    public List<CustomerMapper> seAlleKontiMedBeløb(){
+
+        return null;
+    }
+
+    public List<CustomerMapper> seAlleKonti() throws SQLException {
         //select * from bank.kunde;
         Connection connection = ConnectionDB.getConnection();
         String sql = " select * from bank.kunde;";
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
         ResultSet resultSet = preparedStatement.executeQuery();
+        customerMapperList = new ArrayList<>();
         while (resultSet.next()) {
             int id = resultSet.getInt(1);
             String kdNavn = resultSet.getString("kunde_navn");
             String kdBy = resultSet.getString("kunde_by");
+            CustomerMapper customerMapper = new CustomerMapper(id,kdNavn,kdBy);
+            customerMapperList.add(customerMapper);
             System.out.println("ID" + " " + id + " " + kdNavn + " " + kdBy);
         }
-
+        Collections.sort(customerMapperList);
+        System.out.println(customerMapperList);
+return customerMapperList;
 
     }
 
